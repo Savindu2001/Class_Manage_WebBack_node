bd78724a1da7e770c5e551e8521bcc7659c418bb
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2024-02-10 13:02:05 +0530

    test file testing

--- models/user.js
+++ models/user.js
@@ -1,16 +1,41 @@
-const UserSchema = new UserSchema({
-    first_name:{
-        type:String,
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
     },
-    last_name:{
-        type:String,
+    last_name: {
+        type: String,
     },
-    email:{
-        type:String,
-        required: true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: /^\S+@\S+\.\S+$/,
     },
-    password:{
-        type:String,
-        required: true
+    password: {
+        type: String,
+        required: true,
+        minlength: 6, // Minimum length of the password
+    },
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
     }
-})
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
